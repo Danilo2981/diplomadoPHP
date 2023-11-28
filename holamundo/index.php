dd09d49e311044dfd495ef1d4432786ca5b05457
Danilo Vega <danilo.vega@hotmail.es>
2023-11-27 22:44:02 -0500

    comments

---
+++ holamundo/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Este es un comentario
+#  Este es otro comentario
+/**
+ * Este es un comentario en bloque 
+ * puede contener multiples lineas
+ */
+
+echo "Hola mundo"; #Otro comentario
+
+// Se pueden realizar comentarios en cualquier linea
