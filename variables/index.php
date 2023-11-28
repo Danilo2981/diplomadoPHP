4e57f1dbf74edb163487e3181ce5226828e18478
Danilo Vega <danilo.vega@hotmail.es>
2023-11-27 23:18:04 -0500

    variables

---
+++ variables/index.php
@@ -0,0 +1,28 @@
+<?php
+
+//Variables
+
+$numero = 8;
+$nombre = "Diego";
+$altura = 1.72;
+
+/** Las variables se crean en el momento que se le asigna un valor
+ * 
+ *$edad;
+ *echo $edad;
+ *Dara error ya que no a sido inicializada la variable o asignada un valor 
+ *
+ */
+
+//Lo correcto es inicializar la variable
+
+$edad;
+$edad = 22;
+echo $edad;
+
+// Resultado 980, las variables no tienen separacion
+
+echo $x=9;
+echo $y=8;
+echo $z=0;
+
