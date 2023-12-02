67d626da3eb23448e01b841edb096a0a2a0a26d6
Danilo <danilo.vega@hotmail.es>
2023-12-02 09:47:08 -0500

    Operadores Aritmeticos

---
+++ operadores/aritmericos.php
@@ -0,0 +1,62 @@
+<?php
+
+echo "<h4>Cambio de signo</h4>";
+
+echo -(-5) . "<br>" . "<br>";
+
+$x = -5;
+echo -$x . "<br>";
+
+echo "<h4>Operador de Suma</h4>";
+
+echo 5 + 7 . "<br>" . "<br>";
+
+$numero1 = 7;
+$numero2 = 5;
+echo $numero1 + $numero2 . "<br>";
+
+echo "<h4>Operador de Resta</h4>";
+
+echo 84 - 47 . "<br>" . "<br>";
+
+$numero1 = 84;
+$numero2 = 47;
+echo $numero1 - $numero2 . "<br>";
+
+echo "<h4>Operador de Multiplicación</h4>";
+
+echo 5 * 9 . "<br>" . "<br>";
+
+$numero1 = 5;
+$numero2 = 9;
+echo $numero1 * $numero2 . "<br>";
+
+echo "<h4>Operador de División</h4>";
+
+echo 18 / 4 . "<br>" . "<br>";
+
+$numero1 = 18;
+$numero2 = 4;
+echo $numero1 / $numero2 . "<br>";
+
+echo "<h4>Operador de Módulo</h4>";
+
+echo 9 % 4 . "<br>" . "<br>";
+
+$numero1 = 9;
+$numero2 = 4;
+echo $numero1 % $numero2 . "<br>";
+
+echo "<h4>Operador Exponencial</h4>";
+
+echo 3 ** 5 . "<br>" . "<br>";
+
+$numero1 = 3;
+$numero2 = 5;
+echo $numero1 ** $numero2 . "<br>";
+
+
+
+
+
+
