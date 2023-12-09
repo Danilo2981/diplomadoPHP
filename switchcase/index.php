e810b2bda49a301988b685919dead0ec030e8cfa
Danilo Vega PC <danilo.vega@hotmail.es>
2023-12-09 17:29:50 -0500

    switch anidado

---
+++ switchcase/index.php
@@ -0,0 +1,124 @@
+<?php
+
+/** COMPARARCION ENTRE ELSEIF Y SWITCH */
+
+// Switch Case
+
+$x = 1;
+
+if ($x == 0) {
+    echo "X es igual a 0";
+}
+elseif ($x == 1) {
+    echo "X es igual a 1 <br> <br>";
+}
+elseif ($x === 2) {
+    echo "X es igual a 2 <br> <br>";
+}
+
+// Cambio de elseif con Switch
+
+$x = 1;
+
+switch ($x)
+{
+    case 0:
+        echo "X es igual a 0";
+        break;
+    case 1:
+        echo "X es igual a 1 <br> <br>";
+        break;    
+    case 2:
+        echo "X es igual a 2";
+        break;    
+}
+//break rompe la evaluacion de los siguientes case cuando es encontrado, en elseif se analiza todas las sentencias 
+//asi ya haya encontrado el valor
+
+//Switch permite strings en los case
+
+$fruta = "Mango";
+
+switch ($fruta)
+{
+    case "Manzana":
+        echo "La fruta es una Manzana";
+        break;
+    case "Mango":
+        echo "La fruta es un Mango <br> <br>";
+        break;    
+    case "Pera":
+        echo "La fruta es una Pera";
+        break;    
+}
+
+//Default es el que se ejecuta si no se da ningun case
+
+$diasemana = "Sabado";
+$mensaje = "Error";
+
+switch ($diasemana) {
+    case 'Lunes';
+    case 'Martes';
+    case 'Miércoles';
+    case 'Jueves';
+    case 'Viernes':
+        $mensaje = "día laborable";
+        break;
+    case 'Sabado';
+    case 'Domingo':
+        $mensaje = "día de descanso <br> <br>";
+        break;
+    default:
+        $mensaje = "día invalido";
+        break;
+}
+
+echo $diasemana . " es un " . $mensaje;
+
+// Otro ejemplo de default
+
+$nombre = "Alejandro";
+
+switch ($nombre) {
+    case 'Nicolas':
+        echo "Hola Nicolas";
+        break;
+    case 'Jehison':
+        echo "Hola Jehison";
+        break;
+    case 'Diego':
+        echo "Hola Jehison";
+        break;
+    default:
+        echo "Hola sea quien seas <br> <br>";
+}
+
+// Switch case anidado
+
+$x = 8;
+$y = 4;
+
+$operacion = "/";
+
+switch ($operacion) {
+    case "+":
+        $suma = $x + $y;
+        echo "Resultado de la suma: " . $suma;
+        break;
+    case "-":
+        $resta = $x - $y;
+        echo "Resultado de la resta: " . $resta;
+        break;
+    case "*":
+        $multiplicacion = $x * $y;
+        echo "Resultado de la multiplicacion: " . $multiplicacion;
+        break;
+    case "/":
+        $division = $x / $y;
+        echo "Resultado de la division: " . $division . "<br> <br>";
+        break;
+    default:
+        echo "La operacion no es valida";
+        break;
+}
