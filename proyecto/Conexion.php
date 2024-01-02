8fd4e5a08872b146d9244d5990cbcece433df272
Danilo Vega <danilo.vega@hotmail.es>
2024-01-02 01:02:15 -0500

    MVC Completo

--- proyecto/Conexion.php
+++ proyecto/Conexion.php
@@ -3,15 +3,15 @@
 class Conexion
 {
     protected $db;
-    private $drive = "mysql";
+    private $driver = "mysql";
     private $host = "localhost";
-    private $dbname = "notas";
+    private $bd = "notas";
     private $usuario = "root";
     private $contrasena = "";
 
     public function __construct(){
         try {
-            $db = new PDO("{$this->drive}:host={$this->host};dbname={$this->dbname}", $this->usuario, $this->contrasena);
+            $db = new PDO("{$this->driver}:host={$this->host};dbname={$this->bd}", $this->usuario, $this->contrasena);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $db;
         } catch (PDOException $e) {
